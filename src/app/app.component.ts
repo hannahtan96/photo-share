bd687c9c8dc08930c5cb833e0ca1cde559c2e8fa
hannahtan96 <hannahtan96@gmail.com>
2023-02-03 16:33:31 -0800

    minor name edits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'photo-share-app';
+  title = 'photo-share';
 
   constructor(private _authService: AuthService, private router: Router) {}
 
